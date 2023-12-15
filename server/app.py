44f390a3990401dc6c8e83bea6dd8f4fd030cde7
KariukiSAN <kariukian1@gmail.com>
2023-12-15 09:26:17 +0300

    in progress

--- server/app.py
+++ server/app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request, session, redirect
+import os
+from flask import Flask, render_template, request, send_from_directory, session, redirect
 from database import create_table, insert_user, verify_credentials
 
 app = Flask(__name__)
+#app = Flask(__name__, static_folder='static', template_folder='static/build')
+
+#Need to discuss this
 app.secret_key = 'your_secret_key'  # Set a secret key for session encryption. TBD
 
 create_table()  # Call the create_table function to create the users table
 
-# DefiNING the routes for login and signup pages
+# Defining the routes for login and signup pages
 @app.route('/')
 def landing():
     return render_template('landing.html')
@@ -21,7 +25,7 @@ def login():
         if verify_credentials(username, password):
             # User authentication succeeded
             session['username'] = username  # Store the username in the session
-            return redirect('/')
+            return redirect('/dashboard')  # Redirect the user to the dashboard page
         else:
             # User authentication failed
             return "Invalid credentials. Please try again."
@@ -35,15 +39,26 @@ def signup():
         email = request.form['email']
         password = request.form['password']
         
-        # Perform form validation and additional registration logic here
-        # For example, check for unique usernames or password strength requirements
 
-        # Insert the user information into the database
+
+        
         insert_user(username, email, password)
 
-        return "User registered successfully!"
+        session['username'] = username  
+        return redirect('/dashboard')  
     else:
         return render_template('signup.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return send_from_directory(os.path.join(app.static_folder, 'build'), 'index.html')
+
+@app.route('/static/js/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(os.path.join(app.static_folder, 'build', 'static', 'js'), filename)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+    
