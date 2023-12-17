7826484c57da876d2c228f24cd64136f8bbf6e57
Nick Mugo <nicknjuru13@gmail.com>
2023-12-17 22:59:32 +0300

    added group chat as a diary

--- server/app.py
+++ server/app.py
@@ -1,16 +1,17 @@
 import os
 from flask import Flask, render_template, request, send_from_directory, session, redirect
 from database import create_table, insert_user, verify_credentials
+from flask_cors import CORS
+from flask_migrate import Migrate
+from database import db, Message
+from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
-#app = Flask(__name__, static_folder='static', template_folder='static/build')
 
-#Need to discuss this
-app.secret_key = 'your_secret_key'  # Set a secret key for session encryption. TBD
+app.secret_key = 'your_secret_key' 
 
-create_table()  # Call the create_table function to create the users table
+create_table()  
 
-# Defining the routes for login and signup pages
 @app.route('/')
 def landing():
     return render_template('landing.html')
@@ -24,10 +25,9 @@ def login():
         # Verify credentials against the stored user information in the database
         if verify_credentials(username, password):
             # User authentication succeeded
-            session['username'] = username  # Store the username in the session
-            return redirect('/dashboard')  # Redirect the user to the dashboard page
+            session['username'] = username  
+            return redirect('/dashboard') 
         else:
-            # User authentication failed
             return "Invalid credentials. Please try again."
     else:
         return render_template('login.html')
@@ -57,8 +57,54 @@ def dashboard():
 def serve_static(filename):
     return send_from_directory(os.path.join(app.static_folder, 'build', 'static', 'js'), filename)
 
+CORS(app)
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
+def messages_by_id(id):
+    message = Message.query.filter_by(id=id).first()
+    if request.method == 'PATCH':
+        for attribute in request.json:
+            setattr(message, attribute, request.json.get(attribute))
+        db.session.commit()
+        message_to_dict = message.to_dict() 
+        response = jsonify(message_to_dict)
+        response_body = make_response(response, 200)
+        response_body.headers["Content-Type"] = "application/json"
+        return response_body
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        response_body = {'message': 'Delete was successful'}
+        response = make_response(jsonify(response_body), 200)
+        response.headers["Content-Type"] = "application/json"
+        return response
+
+@app.route('/messages', methods=['GET', 'POST'])
+def get_messages():
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        messages_list = [{'id': msg.id, 'body': msg.body, 'created_at': msg.created_at, 'updated_at': msg.updated_at} for msg in messages]
+        response = jsonify(messages_list)
+        return make_response(response, 200)
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.json.get("body"),
+            username=request.json.get("username"),
+            created_at=request.json.get("created_at"),
+            updated_at=request.json.get("updated_at")
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        new_message_to_dict = new_message.to_dict()
+        response = jsonify(new_message_to_dict)
+        return make_response(response, 200)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5555)
+
 
 
     
--- server/database.py
+++ server/database.py
@@ -1,5 +1,24 @@
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class Message(db.Model, SerializerMixin):
+    __tablename__ = 'messages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    body=db.Column(db.String)
+    username=db.Column(db.String)
+    created_at=db.Column(db.DateTime ,default=datetime.utcnow)
+    updated_at=db.Column(db.DateTime)
 
 def create_table():
     # Connect to the database
