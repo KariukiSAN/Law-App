db662b7d3089b1bb5bd138cb4fbbc2aa216ab96e
KariukiSAN <kariukian1@gmail.com>
2023-12-18 22:09:51 +0300

    react inp

--- lawapplication/src/App.js
+++ lawapplication/src/App.js
@@ -1,20 +1,53 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
+import React, { useState } from 'react';
+
+import './App.css';
+
 
 function App() {
+  const [service, setService] = useState('service1');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+  const bookAppointment = (e) => {
+    e.preventDefault();
+    
+    // Perform the booking logic here
+    // For demonstration purposes, let's assume the booking was successful
+    setSuccessMessage('Appointment booked successfully!');
+    setTimeout(() => {
+      setSuccessMessage('');
+    }, 5000); // Hide the success message after 5 seconds
+  };
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-        <Route path="/dashboard" component={Dashboard} />
-      </Switch>
-    </Router>
+    <div>
+      <h1>Law-App!</h1>
+
+      <div className="card">
+        <h2>Book Services/Appointment</h2>
+        <form onSubmit={bookAppointment}>
+          <label htmlFor="service">Select Service:</label>
+          <select id="service" name="service" value={service} onChange={(e) => setService(e.target.value)}>
+            <option value="service1">Service 1</option>
+            <option value="service2">Service 2</option>
+            <option value="service3">Service 3</option>
+          </select>
+
+          <label htmlFor="date">Select Date:</label>
+          <input type="date" id="date" name="date" required value={date} onChange={(e) => setDate(e.target.value)} />
+
+          <label htmlFor="time">Select Time:</label>
+          <input type="time" id="time" name="time" required value={time} onChange={(e) => setTime(e.target.value)} />
+
+          <button type="submit">Book Now</button>
+        </form>
+      </div>
+
+      {successMessage && (
+        <div className="success-message">{successMessage}</div>
+      )}
+    </div>
   );
 }
 
